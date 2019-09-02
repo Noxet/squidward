c54c863d7452dff21e9bbb2509c33f3ed616dcf8
Jonathan Sönnerup <Nooxet@gmail.com>
2019-09-02 17:28:07 +0200

    Refactored code into multiple files. Problem when compiling, wip

---
+++ esp32-examples/fota_coap/main/fota_coap.h
@@ -0,0 +1,33 @@
+#ifndef FOTA_COAP_H
+#define FOTA_COAP_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_log.h"
+#include "esp_ota_ops.h"
+#include "esp_http_client.h"
+#include "esp_flash_partitions.h"
+#include "esp_partition.h"
+
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+
+#define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
+#define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
+
+const char *TAG = "fota_coap";
+
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+EventGroupHandle_t wifi_event_group;
+/* The event group allows multiple bits for each event,
+   but we only care about one event - are we connected
+   to the AP with an IP? */
+const int CONNECTED_BIT = BIT0;
+
+esp_err_t event_handler(void *, system_event_t *);
+void initialise_wifi(void);
+
+#endif
--- esp32-examples/fota_coap/main/main.c
+++ esp32-examples/fota_coap/main/main.c
@@ -1,31 +1,11 @@
-/* OTA example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-   */
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
-#include "esp_log.h"
-#include "esp_ota_ops.h"
-#include "esp_http_client.h"
-#include "esp_flash_partitions.h"
-#include "esp_partition.h"
 
 #include "driver/gpio.h"
 
 #include "nvs.h"
 #include "nvs_flash.h"
 
-#define EXAMPLE_WIFI_SSID CONFIG_WIFI_SSID
-#define EXAMPLE_WIFI_PASS CONFIG_WIFI_PASSWORD
+#include "fota_coap.h"
+
 #define EXAMPLE_SERVER_URL CONFIG_FIRMWARE_UPG_URL
 #define BUFFSIZE 1024
 #define HASH_LEN 32 /* SHA-256 digest length */
@@ -38,61 +18,13 @@
  */
 #define ESP_INTR_FLAG_DEFAULT 0
 
-static const char *TAG = "fota_coap";
 /*an ota data write buffer ready to write to the flash*/
 static char ota_write_data[BUFFSIZE + 1] = { 0 };
 extern const uint8_t server_cert_pem_start[] asm("_binary_ca_cert_pem_start");
 extern const uint8_t server_cert_pem_end[] asm("_binary_ca_cert_pem_end");
 
-/* FreeRTOS event group to signal when we are connected & ready to make a request */
-static EventGroupHandle_t wifi_event_group;
 static xQueueHandle gpio_evt_queue = NULL;
 
-/* The event group allows multiple bits for each event,
-   but we only care about one event - are we connected
-   to the AP with an IP? */
-const int CONNECTED_BIT = BIT0;
-
-static esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-	switch (event->event_id) {
-		case SYSTEM_EVENT_STA_START:
-			esp_wifi_connect();
-			break;
-		case SYSTEM_EVENT_STA_GOT_IP:
-			xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-			break;
-		case SYSTEM_EVENT_STA_DISCONNECTED:
-			/* This is a workaround as ESP32 WiFi libs don't currently
-			   auto-reassociate. */
-			esp_wifi_connect();
-			xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
-			break;
-		default:
-			break;
-	}
-	return ESP_OK;
-}
-
-static void initialise_wifi(void)
-{
-	tcpip_adapter_init();
-	wifi_event_group = xEventGroupCreate();
-	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-	wifi_config_t wifi_config = {
-		.sta = {
-			.ssid = EXAMPLE_WIFI_SSID,
-			.password = EXAMPLE_WIFI_PASS,
-		},
-	};
-	ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
-	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-	ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-	ESP_ERROR_CHECK( esp_wifi_start() );
-}
 
 static void http_cleanup(esp_http_client_handle_t client)
 {
