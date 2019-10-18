30f10458c4fbfb8257e0b5ba230e6bb7e502ee9e
Jonathan Sönnerup <Nooxet@gmail.com>
2019-10-18 16:14:11 +0200

    Convert to tabs

--- src/mqtts/main/mqtts.c
+++ src/mqtts/main/mqtts.c
@@ -46,75 +46,75 @@ extern const uint8_t mqtt_server_ca_pem_end[]   asm("_binary_mqtt_server_ca_pem_
 
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
-    esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
+	esp_mqtt_client_handle_t client = event->client;
+	int msg_id;
 
 #define ANT_BUF_SIZE 64
 	char annotation_msg[ANT_BUF_SIZE];
 
-    // your_context_t *context = event->context;
-    switch (event->event_id) {
-        case MQTT_EVENT_CONNECTED:
+	// your_context_t *context = event->context;
+	switch (event->event_id) {
+		case MQTT_EVENT_CONNECTED:
 #ifdef CONFIG_SQ_MAIN_DBG
-            ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+			ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
 #endif
-            msg_id = esp_mqtt_client_subscribe(client, "/updates", 0);
+			msg_id = esp_mqtt_client_subscribe(client, "/updates", 0);
 #ifdef CONFIG_SQ_MAIN_DBG
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+			ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 #endif
 
-            //msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-            //ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+			//msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+			//ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-            //msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-            //ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
-            break;
-        case MQTT_EVENT_DISCONNECTED:
+			//msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+			//ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+			break;
+		case MQTT_EVENT_DISCONNECTED:
 #ifdef CONFIG_SQ_MAIN_DBG
-            ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+			ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
 #endif
-            break;
+			break;
 
-        case MQTT_EVENT_SUBSCRIBED:
+		case MQTT_EVENT_SUBSCRIBED:
 #ifdef CONFIG_SQ_MAIN_DBG
-            ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+			ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
 #endif
 
 			while (mqtt_data_len <= MQTT_DATA_BUF_SIZE) {
-                snprintf(annotation_msg, ANT_BUF_SIZE, "%s %d bytes\n", ant_pub, mqtt_data_len);
-                sq_uart_send(annotation_msg, strlen(annotation_msg));
+				snprintf(annotation_msg, ANT_BUF_SIZE, "%s %d bytes\n", ant_pub, mqtt_data_len);
+				sq_uart_send(annotation_msg, strlen(annotation_msg));
 				msg_id = esp_mqtt_client_publish(client, MQTT_TOPIC, mqtt_data, mqtt_data_len, 0, 0);
 				sq_uart_send(ant_pub_done, sizeof(ant_pub_done));
 #ifdef CONFIG_SQ_MAIN_DBG
-                ESP_LOGI(TAG, "published %d byte(s) of data", mqtt_data_len);
-                ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+				ESP_LOGI(TAG, "published %d byte(s) of data", mqtt_data_len);
+				ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 #endif
 				/* double the size for next message */
 				mqtt_data_len *= 2;
 				sleep(1);
 			}
-            break;
-        case MQTT_EVENT_UNSUBSCRIBED:
-            ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-            break;
-        case MQTT_EVENT_PUBLISHED:
+			break;
+		case MQTT_EVENT_UNSUBSCRIBED:
+			ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+			break;
+		case MQTT_EVENT_PUBLISHED:
 #ifdef CONFIG_SQ_MAIN_DBG
-            ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+			ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
 #endif
-            break;
-        case MQTT_EVENT_DATA:
-            ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-            printf("DATA=%.*s\r\n", event->data_len, event->data);
-            break;
-        case MQTT_EVENT_ERROR:
-            ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-            break;
-        default:
-            ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-            break;
-    }
-    return ESP_OK;
+			break;
+		case MQTT_EVENT_DATA:
+			ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+			printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+			printf("DATA=%.*s\r\n", event->data_len, event->data);
+			break;
+		case MQTT_EVENT_ERROR:
+			ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+			break;
+		default:
+			ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+			break;
+	}
+	return ESP_OK;
 }
 
 
@@ -128,34 +128,34 @@ static void mqtt_app_start(void)
 	ESP_LOGI(TAG, "[%s] - Connected to WiFi", __FUNCTION__);
 #endif
 
-    const esp_mqtt_client_config_t mqtt_cfg = {
-        .uri = CONFIG_BROKER_URI,
-        .event_handle = mqtt_event_handler,
-        .cert_pem = (const char *)mqtt_server_ca_pem_start,
-    };
+	const esp_mqtt_client_config_t mqtt_cfg = {
+		.uri = CONFIG_BROKER_URI,
+		.event_handle = mqtt_event_handler,
+		.cert_pem = (const char *)mqtt_server_ca_pem_start,
+	};
 
 	/* initialize data buffer */
 	for (int i = 0; i < MQTT_DATA_BUF_SIZE; i++) {
 		mqtt_data[i] = 'a';
 	}
 
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_start(client);
+	ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+	esp_mqtt_client_start(client);
 }
 
 void app_main()
 {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+	ESP_LOGI(TAG, "[APP] Startup..");
+	ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+	ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+	esp_log_level_set("*", ESP_LOG_INFO);
+	esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
+	esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+	esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
 
 #ifndef CONFIG_SQ_MAIN_DBG
 	/* Turn of all debugging. */
@@ -165,8 +165,8 @@ void app_main()
 
 	sq_uart_init();
 
-    nvs_flash_init();
-    wifi_init();
-    mqtt_app_start();
+	nvs_flash_init();
+	wifi_init();
+	mqtt_app_start();
 
 }
