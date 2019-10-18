14fa259ace9f1a34e875c641ad6c5461b51565fd
Jonathan Sönnerup <Nooxet@gmail.com>
2019-10-18 16:13:48 +0200

    Added PKI support

--- src/mqtts/main/component.mk
+++ src/mqtts/main/component.mk
@@ -1,1 +1,2 @@
-COMPONENT_EMBED_TXTFILES := iot_eclipse_org.pem
+#COMPONENT_EMBED_TXTFILES := iot_eclipse_org.pem
+COMPONENT_EMBED_TXTFILES := mqtt_server_ca.pem
--- src/mqtts/main/mqtts.c
+++ src/mqtts/main/mqtts.c
@@ -24,48 +24,83 @@
 
 const char *TAG = "mqtts";
 
+#define MQTT_TOPIC "/squidward"
+
+#define MQTT_DATA_BUF_SIZE (16 * 1024)
+static char mqtt_data[MQTT_DATA_BUF_SIZE];
+static int mqtt_data_len = 1;
+
 EventGroupHandle_t wifi_event_group;
 const int CONNECTED_BIT = BIT0;
 
+const char ant_pub[]		= "MQTT publish";
+const char ant_pub_done[]	= "MQTT publish done\n";
+
 
 #if CONFIG_BROKER_CERTIFICATE_OVERRIDDEN == 1
-static const uint8_t iot_eclipse_org_pem_start[]  = "-----BEGIN CERTIFICATE-----\n" CONFIG_BROKER_CERTIFICATE_OVERRIDE "\n-----END CERTIFICATE-----";
+static const uint8_t mqtt_server_ca_pem_start[]  = "-----BEGIN CERTIFICATE-----\n" CONFIG_BROKER_CERTIFICATE_OVERRIDE "\n-----END CERTIFICATE-----";
 #else
-extern const uint8_t iot_eclipse_org_pem_start[]   asm("_binary_iot_eclipse_org_pem_start");
+extern const uint8_t mqtt_server_ca_pem_start[]   asm("_binary_mqtt_server_ca_pem_start");
 #endif
-extern const uint8_t iot_eclipse_org_pem_end[]   asm("_binary_iot_eclipse_org_pem_end");
+extern const uint8_t mqtt_server_ca_pem_end[]   asm("_binary_mqtt_server_ca_pem_end");
 
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 {
     esp_mqtt_client_handle_t client = event->client;
     int msg_id;
+
+#define ANT_BUF_SIZE 64
+	char annotation_msg[ANT_BUF_SIZE];
+
     // your_context_t *context = event->context;
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
+#ifdef CONFIG_SQ_MAIN_DBG
             ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+#endif
+            msg_id = esp_mqtt_client_subscribe(client, "/updates", 0);
+#ifdef CONFIG_SQ_MAIN_DBG
             ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+#endif
 
-            msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-            ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+            //msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+            //ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-            msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-            ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+            //msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+            //ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
             break;
         case MQTT_EVENT_DISCONNECTED:
+#ifdef CONFIG_SQ_MAIN_DBG
             ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+#endif
             break;
 
         case MQTT_EVENT_SUBSCRIBED:
+#ifdef CONFIG_SQ_MAIN_DBG
             ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-            msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-            ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+#endif
+
+			while (mqtt_data_len <= MQTT_DATA_BUF_SIZE) {
+                snprintf(annotation_msg, ANT_BUF_SIZE, "%s %d bytes\n", ant_pub, mqtt_data_len);
+                sq_uart_send(annotation_msg, strlen(annotation_msg));
+				msg_id = esp_mqtt_client_publish(client, MQTT_TOPIC, mqtt_data, mqtt_data_len, 0, 0);
+				sq_uart_send(ant_pub_done, sizeof(ant_pub_done));
+#ifdef CONFIG_SQ_MAIN_DBG
+                ESP_LOGI(TAG, "published %d byte(s) of data", mqtt_data_len);
+                ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+#endif
+				/* double the size for next message */
+				mqtt_data_len *= 2;
+				sleep(1);
+			}
             break;
         case MQTT_EVENT_UNSUBSCRIBED:
             ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
             break;
         case MQTT_EVENT_PUBLISHED:
+#ifdef CONFIG_SQ_MAIN_DBG
             ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+#endif
             break;
         case MQTT_EVENT_DATA:
             ESP_LOGI(TAG, "MQTT_EVENT_DATA");
@@ -85,11 +120,24 @@ static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event)
 
 static void mqtt_app_start(void)
 {
+
+	/* wait for WiFi connection */
+	xEventGroupWaitBits(wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+
+#ifdef CONFIG_SQ_MAIN_DBG
+	ESP_LOGI(TAG, "[%s] - Connected to WiFi", __FUNCTION__);
+#endif
+
     const esp_mqtt_client_config_t mqtt_cfg = {
         .uri = CONFIG_BROKER_URI,
         .event_handle = mqtt_event_handler,
-        .cert_pem = (const char *)iot_eclipse_org_pem_start,
+        .cert_pem = (const char *)mqtt_server_ca_pem_start,
     };
+
+	/* initialize data buffer */
+	for (int i = 0; i < MQTT_DATA_BUF_SIZE; i++) {
+		mqtt_data[i] = 'a';
+	}
 
     ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
@@ -108,6 +156,14 @@ void app_main()
     esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
     esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
     esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+#ifndef CONFIG_SQ_MAIN_DBG
+	/* Turn of all debugging. */
+	ESP_LOGW(TAG, "Turning off all debugging logs.");
+	esp_log_level_set("*", ESP_LOG_NONE);
+#endif
+
+	sq_uart_init();
 
     nvs_flash_init();
     wifi_init();
